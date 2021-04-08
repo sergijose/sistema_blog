e6b5c0c4b908eeee6094dea495dd66df99b5d622
sergio jara <sergiojosel.j@gmail.com>
2021-04-07 21:08:37 -0500

    maquetando vista de blog

--- cms/resources/views/paginas/blog.blade.php
+++ cms/resources/views/paginas/blog.blade.php
@@ -119,6 +119,7 @@
                           $tags= json_decode($element->palabras_claves,true);
                          $palabras_claves="";
                          foreach ($tags as $key => $value) {
+                           
                            $palabras_claves.=$value.",";
                          }
                         
@@ -173,25 +174,31 @@
 
                       <div class="row">
 
-                        <div class="col-lg-6">
-    
-                          <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                              <div class="input-group-text text-white" style="background:#1475E0">
-    
-                                <i class="fab fa-facebook-f"></i>
+                        @php
+                        $redes= json_decode($element->redes_sociales,true);
+                       
+                      foreach ($redes as $key => $value) {
+                       echo  '<div class="col-lg-12">
+                              <div class="input-group mb-3">
+                             <div class="input-group-prepend">
+                            <div class="input-group-text text-white" style="background:'.$value["background"].'">
+                            <i class="'.$value["icono"].'"></i>
                               </div>
-                            </div>
-                            <input type="text" class="form-control" value="https:/www.facebook.com">
+                             </div>
+                            <input type="text" class="form-control" value="'.$value["url"].'">
                             <div class="input-group-prepend">
-                              <div class="input-group-text" style="cursor:pointer">
-                              <span class="bg-danger px-2 rounded-circle">&times;</span>  
-    
-                               
-                              </div>
+                             <div class="input-group-text" style="cursor:pointer">
+                             <span class="bg-danger px-2 rounded-circle">&times;</span>  
+                             </div>
                             </div>
-                          </div>
-                        </div>
+                           </div>
+                          </div>';
+                       
+                       }
+                      
+                      @endphp
+
+                       
                       </div>
                   </div>
 
@@ -206,13 +213,82 @@
                 <div class="card">
 
                   <div class="card-body">
-                    hola
+                    <div class="row">
+                      <div class="col-lg-12">
+
+                        {{-- cambiar logo --}}
+
+                        <div class="form-group my-2 text-center">
+                          <div class="btn btn-default btn-file mb-3">
+
+                            <i class="fas fa-paperclip"></i>Adjuntar imagen de logo
+                            <input type="file" name="logo">
+                            </div>
+                            <img src="{{url('/')}}/{{$element->logo}}" class="img-fluid py-2 bg-secondary">
+                            <p class="help-block small mt-3">Dimensiones 700px * 200px| Peso maximo 2MB|Formato JPG o PNG</p>
+                         
+                        </div>
+                        <hr class="pb-2">
+
+                          {{-- CAMBIAR PORTADA --}}
+
+                          <div class="form-group my-2 text-center">
+                            <div class="btn btn-default btn-file mb-3">
+  
+                              <i class="fas fa-paperclip"></i>Adjuntar imagen de portada
+                              <input type="file" name="portada">
+                              </div>
+                              <img src="{{url('/')}}/{{$element->portada}}" class="img-fluid py-2 bg-secondary">
+                              <p class="help-block small mt-3">Dimensiones 700px * 420px| Peso maximo 2MB|Formato JPG o PNG</p>
+                           
+                          </div>
+                          <hr class="pb-2">
+
+                          {{-- CAMBIAR ICONO --}}
+
+                          <div class="form-group my-2 text-center">
+                            <div class="btn btn-default btn-file mb-3">
+  
+                              <i class="fas fa-paperclip"></i>Adjuntar imagen de icono
+                              <input type="file" name="icono">
+                              </div>
+                              <br>
+                              <img src="{{url('/')}}/{{$element->icono}}" class="img-fluid py-2  rounded-circle">
+                              <p class="help-block small mt-3">Dimensiones 150px * 150px| Peso maximo 2MB|Formato JPG o PNG</p>
+                           
+                          </div>
+                      </div>
+
+
+                    </div>
                     
                   </div>
                 </div>
 
 
               </div>
+
+              <div class="col-lg-6">
+                  <div class="card">
+
+                     <div class="card-body">
+                       <label >Sobre Mi <span class="small">(Intro)</span></label>
+                      <textarea class="form-control" name="sobre_mi"">{{ $element->sobre_mi}} }}</textarea>
+                    </div>
+                </div>
+              </div>
+
+              <div class="col-lg-6">
+                 <div class="card">
+
+                    <div class="card-body">
+                      <label >Sobre Mi <span class="small">(Completo)</span></label>
+                      <textarea class="form-control" name="sobre_mi_completo"">{{ $element->sobre_mi_completo}} }}</textarea>
+                    </div>
+                </div>
+              </div>
+
+
 
             </div>
             
